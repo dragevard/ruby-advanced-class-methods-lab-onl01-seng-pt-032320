97ca0fee2ae8fffbfd103f1fc9551b989b3035f3
Derek phung <dragevards@gmail.com>
2020-04-24 23:12:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,9 +37,9 @@ class Song
   
   def self.find_or_create_by_name(song_name)
     if self.find_by_name == nil 
-      self.create_by_name 
+      self.create_by_name(song_name) 
     else 
-      self.find_by_name
+      self.find_by_name(song_name)
     end 
   end
   
